9a8bc7e287db8892d1c6989c65d33c38a23e3df5
Danilo Canguçu <daniloccangucu@gmail.com>
2025-04-04 18:49:23 +0300

    Add GET /show/[showSlug] to api

---
+++ pages/api/show/[showSlug].ts
@@ -0,0 +1,17 @@
+import { getShow } from "@/app/services/showService";
+import { sanitizeShow } from "@/app/utils/showUtils";
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { showSlug } = req.query;
+
+  try {
+    const showDetails = await getShow(showSlug as string);
+    const mappedShow = sanitizeShow(showDetails);
+    res.status(200).json(mappedShow);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch show details" });
+  }
+}
