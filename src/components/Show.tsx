8cbb7c26c09de427685e270f883a4d1991150399
Danilo Canguçu <daniloccangucu@gmail.com>
2025-04-08 18:25:10 +0300

    WIP: Show page & component

---
+++ src/components/Show.tsx
@@ -0,0 +1,21 @@
+import { ShowDTO } from "@/types/Show";
+import styles from '../styles/Show.module.css';
+
+
+interface ShowProps {
+  showData: ShowDTO;
+}
+
+const Show = ({ showData }: ShowProps) => {
+  return (
+    <main className={styles.show}>
+      <section className="show-header show">
+        <h1 className="show-title">{showData.title}</h1>
+        <p className="show-tagline">{showData.tagline}</p>
+        <p className="show-description">{showData.description}</p>
+      </section>
+    </main>
+  );
+};
+
+export default Show;
