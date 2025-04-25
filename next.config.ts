ec119ebd194b54a8a358c0721eb0b21c898ad3fb
Danilo Canguçu <daniloccangucu@gmail.com>
2025-04-25 11:13:11 +0200

    Update: provisional image domain in config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    // Provisional domain for images
+    domains: ["danilocangucu.net"],
+  },
 };
 
 export default nextConfig;
