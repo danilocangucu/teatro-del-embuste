a7ab2d492e53a13d5a6fbfc2cbd5a48912ec52a1
Danilo Canguçu <daniloccangucu@gmail.com>
2025-05-28 14:17:25 +0300

    Update constants.ts with shopSteps and ticketsPath

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,7 +5,21 @@ export const API_BASE_URL =
 // STYLES
 export const browserDefaultMargin = "8px";
 
+// IMAGES & ICONS
 export const arrowRightUp = "/arrow-up-right-1.svg";
 export const arrowRightUpWhite = "/arrow-up-right-white.svg";
 export const logo = "/logo.png";
 export const logoFooter = "/logo_footer.png";
+
+// ROUTES
+export const ticketsPath = "/boletas";
+
+// TICKETS
+export const shopSteps = [
+    "Boletas",
+    "Identidad",
+    "Revisión",
+    "Pago",
+    "Verificación",
+    "Confirmación",
+  ];
