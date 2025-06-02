b7bb964d46d11f174924aba49717723abf3dbe8e
Danilo Canguçu <daniloccangucu@gmail.com>
2025-06-02 16:15:45 +0300

    Update PollingReservation to use dynamic slugs

--- src/components/Ticket/PollingReservation.tsx
+++ src/components/Ticket/PollingReservation.tsx
@@ -3,63 +3,47 @@
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
-// TODO temporary reservation type in PollingReservation.tsx
-type Reservation = {
-  id: string;
-  status: string;
-  // add other fields if needed
-};
 
-export function PollingReservation({ boldOrderId }: { boldOrderId: string }) {
-  const [reservation, setReservation] = useState<Reservation | null>(null);
+export function PollingReservation({ boldOrderIdWithoutSufix }: { boldOrderIdWithoutSufix: string }) {
   const [attempts, setAttempts] = useState(0);
-  const [status, setStatus] = useState<number | null>(null);
+  const [foundReservation, setFoundReservation] = useState(false);
+  const [reservationStatus, setReservationStatus] = useState<string | null>(null);
 
   const router = useRouter();
   const maxAttempts = 5;
   const intervalMs = 4000;
 
   useEffect(() => {
-    if (reservation || attempts >= maxAttempts) return;
+    if (attempts >= maxAttempts) return;
 
     const timer = setTimeout(async () => {
       try {
         console.log(
           `"[POLLING RESERVATION] Attempt #${attempts} to fetch reservation for boldOrderId:"`,
-          boldOrderId
+          boldOrderIdWithoutSufix
         );
-        const res = await fetch(`/api/tickets/reservations?id=${boldOrderId}`);
+        const res = await fetch(`/api/tickets/reservations?id=${boldOrderIdWithoutSufix}`);
         console.log("[POLLING RESERVATION] Fetching reservation...");
         console.log("[POLLING RESERVATION] Response status:", res.status);
-        console.log("[POLLING RESERVATION] Response ok:", res.ok);
         if (res.ok) {
           const data = await res.json();
-          if (data.status === 400) {
-            console.warn(
-              "[POLLING RESERVATION] No reservation found in cookie"
-            );
-            setStatus(400);
-            return;
-          }
           console.log("[POLLING RESERVATION] Response data:", data);
           if (data.reservation) {
+            console.log("[POLLING RESERVATION] Reservation found:", data.reservation);
+
             if (data.reservation.status === "confirmed" || data.reservation.status === "cancelled") {
               console.log(
                 "[POLLING RESERVATION] Reservation confirmed, stopping polling."
               );
               clearTimeout(timer);
               setAttempts(maxAttempts);
-              // TODO redirect for now needs showSlug and performanceSlug and its hardcoded.
-              router.push(`/boletas/la-secreta/07-06-25-6PM/confirmacion`);
+              router.push(`/boletas/${data.reservation.showSlug}/${data.reservation.performanceSlug}/confirmacion`);
             }
-            console.log(
-              "[POLLING RESERVATION] Reservation found, updating state."
-            );
-            setReservation(data.reservation);
-          }
+            setFoundReservation(true);
+            setReservationStatus(data.reservation.status);
+          };
           setAttempts(attempts + 1);
         } else {
-          console.log("[POLLING RESERVATION] !Res.ok:");
           setAttempts(attempts + 1);
         }
       } catch (error) {
@@ -72,40 +56,9 @@ export function PollingReservation({ boldOrderId }: { boldOrderId: string }) {
     }, intervalMs);
 
     return () => clearTimeout(timer);
-  }, [reservation, attempts, boldOrderId, router]);
+  }, [attempts, boldOrderIdWithoutSufix, router]);
 
-  if (reservation) {
-    console.log("[POLLING RESERVATION] Reservation found:", reservation);
-    return (
-      <div style={{ marginTop: 40, marginLeft: 40, fontFamily: "sans-serif", maxWidth: 500 }}>
-        <h1>Confirmación</h1>
-        <br /><br />
-        <p>Tu compra ha sido encontrada.</p>
-        <br />
-        <p>Cargando detalles...</p>
-        <br />
-      </div>
-    );
-  }
-
-  if (status === 400) {
-    return (
-      <div style={{ marginTop: 40, marginLeft: 40, fontFamily: "sans-serif", maxWidth: 500 }}>
-        <p>
-          No se encontró una reserva en tu sesión. Por favor, verifica que hayas
-          realizado el pago correctamente y vuelve a intentarlo.
-        </p><br /><br />
-        <p>
-          Si el problema persiste, contáctanos por correo al embuste@gmail.com o
-          por teléfono al 3112326206.
-        </p><br />
-        <p>Horario de atención: lunes a viernes, de 9:00 a.m. a 5:00 p.m.</p><br /><br />
-        <p>Gracias por tu paciencia.</p>
-      </div>
-    );
-  }
-
-  if (attempts >= maxAttempts) {
+  if (!foundReservation && attempts >= maxAttempts) {
     console.warn(
       "[POLLING RESERVATION] Max attempts reached, no reservation found."
     );
@@ -116,9 +69,38 @@ export function PollingReservation({ boldOrderId }: { boldOrderId: string }) {
         <br />
         <br />
         <p>
-          Tu pedido aún no ha sido confirmado. Estamos revisando tu pago y te
-          contactaremos por correo o teléfono si compartiste esa información.
-          Revisa tu correo, incluyendo la carpeta de spam, para actualizaciones.
+          No hemos encontrado tu reserva.
+        </p>
+        <br />
+        <br />
+        <p>
+          Si necesitas ayuda, contáctanos por correo a embuste@gmail.com o por
+          teléfono al 3112326206.
+        </p>
+        <br />
+        <p>Horario de atención: lunes a viernes</p>
+        <br />
+        <p>De 9:00 a.m. a 5:00 p.m.</p>
+        <br />
+        <br />
+        <p>Gracias por tu paciencia.</p>
+      </div>
+    );
+  }
+
+  if (foundReservation && reservationStatus && attempts >= maxAttempts) {
+    console.log("[POLLING RESERVATION] attempts:", attempts);
+    console.log("[POLLING RESERVATION] Reservation found, status:", reservationStatus);
+    console.log("[POLLING RESERVATION] Reservation status:", reservationStatus);
+    return (
+      <div style={{ marginTop: 40, marginLeft: 40, fontFamily: "sans-serif", maxWidth: 500 }}>
+        <h1>Estado de la verificación</h1>
+        <br />
+        <br />
+        <br />
+        <br />
+        <p>
+          El pago de tu reserva no ha sido confirmado. El equipo del Teatro del Embuste ha sido notificado y está trabajando para resolver el problema. Por favor, ten paciencia mientras verificamos tu pago. Pronto te contactaremos por correo electrónico o por teléfono si has proporcionado un número de contacto.
         </p>
         <br />
         <br />
